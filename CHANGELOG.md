775a366a4e8260f04450f4543f04b6125aa929d5
Brian Armstrong <brian@flutter.institute>
2018-07-18 23:43:26 -0600

    removing unused import in test files

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+## [1.0.1] - 2018-07-18
+
+* Removing unused import from the test files
+
 ## [1.0.0] - 2018-07-17
 
 * Initial release for sharing location data using InheritedWidget
--- test/location_context_test.dart
+++ test/location_context_test.dart
@@ -3,7 +3,6 @@
 
 import 'dart:async';
 
-import 'package:async/async.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
